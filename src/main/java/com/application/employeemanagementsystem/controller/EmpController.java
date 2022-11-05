c43349988ab017c9f69e1b378c99ca8bef1d0b8a
ridma <ridmakaveendra7@gmail.com>
2022-11-05 12:50:53 +0530

    third

--- src/main/java/com/application/employeemanagementsystem/controller/EmpController.java
+++ src/main/java/com/application/employeemanagementsystem/controller/EmpController.java
@@ -17,46 +17,46 @@ public class EmpController {
     private EmployeeService employeeService;
 
     @GetMapping("/home")
-    public String home(Model model){
+    public String home(Model model) {
 
         List<Employee> employeeList = employeeService.getAllEmployees();
-        model.addAttribute("emp",employeeList);
+        model.addAttribute("emp", employeeList);
         return "index";
     }
 
     @GetMapping("/addemployee")
-    public String addEmployee(){
+    public String addEmployee() {
         return "addemployee";
     }
 
     @PostMapping("/register")
-    public String employeeRegister(@ModelAttribute Employee employee, HttpSession session){
+    public String employeeRegister(@ModelAttribute Employee employee, HttpSession session) {
         System.out.println(employee);
         employeeService.addEmployee(employee);
-        session.setAttribute("msg","Employee Added Successfully..!");
+        session.setAttribute("msg", "Employee Added Successfully..!");
 
         return "redirect:/home";
     }
 
     @GetMapping("/edit/{id}")
-    public String editEmployee(@PathVariable int id, Model model){
+    public String editEmployee(@PathVariable int id, Model model) {
         Employee employee = employeeService.getEmployeeById(id);
         model.addAttribute("emp", employee);
-        return  "edit";
+        return "edit";
     }
 
     @PostMapping("/update")
-    public String updateEmployee(@ModelAttribute Employee employee, HttpSession session){
+    public String updateEmployee(@ModelAttribute Employee employee, HttpSession session) {
         employeeService.addEmployee(employee);
-        session.setAttribute("msg","Employee Details Edited Successfully..!");
+        session.setAttribute("msg", "Employee Details Edited Successfully..!");
 
         return "redirect:/home";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable int id,  HttpSession session){
+    public String delete(@PathVariable int id, HttpSession session) {
         employeeService.deleteEmployee(id);
-        session.setAttribute("msg","Employee Deleted Successfully..!");
+        session.setAttribute("msg", "Employee Deleted Successfully..!");
         return "redirect:/home";
     }
 
