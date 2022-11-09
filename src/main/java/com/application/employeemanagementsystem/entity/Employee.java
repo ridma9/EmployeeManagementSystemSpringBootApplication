6bd8c2297d716d6c1cb80c8ebb638bcfb57d2ea6
ridma <ridmakaveendra7@gmail.com>
2022-11-09 12:05:27 +0530

    nov_9th

--- src/main/java/com/application/employeemanagementsystem/entity/Employee.java
+++ src/main/java/com/application/employeemanagementsystem/entity/Employee.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String name;
     private String address;
